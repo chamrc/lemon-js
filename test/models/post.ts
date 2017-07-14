8086608fe83535e5e02126a1f750115f13ae0068
Jacob Zhang <rc.z@me.com>
2017-07-14 12:51:14 +0900

    Trying to fix ci error

---
+++ test/models/post.ts
@@ -0,0 +1,13 @@
+import { model, property, Query, Ref, TypedModel } from '../../';
+import { User } from './user';
+
+@model
+export class Post extends TypedModel {
+	@property public title: string;
+	@property public body: string;
+	@property({ ref: User }) public creator: User;
+
+	public static findByTitle(title: string): Query<Post> {
+		return this.findOne({ title });
+	}
+}
---
+++ test/models/user.ts
@@ -0,0 +1,20 @@
+import { Permission, Post } from '.';
+import { model, property, Query, Ref, TypedModel } from '../../';
+
+@model
+export class User extends TypedModel {
+	@property public age: number;
+	@property public createdAt: Date;
+	@property({ index: true }) public email: string;
+	@property({ refer: Permission }) public permissions: [Permission];
+	@property({ default: false }) public isActive: boolean;
+	@property({ unique: true }) public name: string;
+
+	public get displayName() {
+		return `${this.name} <${this.email}>`;
+	}
+
+	public static findByEmail(email: string): Query<User> {
+		return this.findOne({ email });
+	}
+}
